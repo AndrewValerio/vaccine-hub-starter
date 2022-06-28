e839005160cf05f8be6020b6b92ce4e7c7584e7d
AndrewValerio <andrewvalerio1396@gmail.com>
2022-06-28 13:26:15 -0700

    connected api to postgres

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -2,16 +2,21 @@ const express =require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 const {PORT} = require("./config")
+const authRoutes = require("./routes/auth")
+var bodyParser = require('body-parser')
 
-const {NotFoundError} = require("./utils/errors")
+const {BadRequestError, NotFoundError} = require("./utils/errors")
 
 const app = express()
 
 app.use(cors());
 
 app.use(express.json())
+app.use(bodyParser.json())
 
 app.use(morgan('tiny'))
+
+app.use("/auth", authRoutes)
 
 app.use((req, res, next) => {
     return next(new NotFoundError())
